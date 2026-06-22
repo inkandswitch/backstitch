3a82c0a321c6b6a169c41b8b562ee7bc72176b81
nikitalita <69168929+nikitalita@users.noreply.github.com>
2026-06-22 12:13:22 -0700

    change `refresh_after_source_change()` to be async

--- rust/src/interop/godot_accessors.rs
+++ rust/src/interop/godot_accessors.rs
@@ -164,10 +164,15 @@ impl BackstitchEditorAccessor {
     }
 
     pub fn refresh_after_source_change() -> bool {
-        if Self::scan_fs_sync() {
-            tracing::warn!("Scanning filesystem timed out!");
-            return false;
-        }
+        // TODO: This isn't necessary and doesn't prevent gdscript crashes, and it significanly slows down syncing
+        // We should probably have a way to force a full scan if we need to, i.e. on a project start or a branch-checkout
+        // if Self::scan_fs_sync() {
+        //     tracing::warn!("Scanning filesystem timed out!");
+        //     return false;
+        // }
+        EditorInterface::singleton()
+            .get_resource_filesystem()
+            .unwrap().scan_sources();
         let mut script_editor = EditorInterface::singleton().get_script_editor().unwrap();
         // TODO: when 4.7 is released, use the bound method instead
         script_editor.call("reload_open_files", &[]);
